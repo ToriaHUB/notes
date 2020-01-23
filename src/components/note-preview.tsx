9659e88b9c626f2d14d136b10abc065ca3575ffe
Viktoria Petrenko <vikuacz@seznam.cz>
2020-01-23 22:21:42 +0100

    NOTES: implemented note's preview component

--- src/components/note-preview.tsx
+++ src/components/note-preview.tsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react"
+import styled from "styled-components"
+import { Button } from "./button"
 
-export const NotesPreview = ()=>{
-  return(
-    <div>
-      NotesPreview
-    </div>
+type Props = {
+  id: string
+  title: string
+  date: string
+}
+
+export const NotePreview: React.FC<Props> = ({ title, date }) => {
+  return (
+    <NoteWrapper>
+      <Title>{title}</Title>
+      <Date>{date}</Date>
+      <Actions>
+        <Button type={"detail"} onClick={() => {}}>
+          Detail
+        </Button>
+        <Button type={"delete"} onClick={() => {}}>
+          Delete
+        </Button>
+      </Actions>
+    </NoteWrapper>
   )
 }
+const NoteWrapper = styled.div`
+  display: flex;
+  width: 60%;
+  height: 40px;
+  justify-content: space-between;
+  padding: 10px;
+  border-radius: 10px;
+  background-color: #f5f5f5;
+  margin-bottom: 20px;
+  align-items: center;
+`
+
+const Title = styled.h2`
+  display: flex;
+  width: 200px;
+`
+const Date = styled.p`
+  display: flex;
+  width: 80px;
+`
+const Actions = styled.div`
+  display: flex;
+  width: 175px;
+  justify-content: space-between;
+`
--- src/components/notes.tsx
+++ src/components/notes.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react"
+import { NotePreview } from "./note-preview"
 
-export const Notes = ()=>{
-  return(
-    <div>
-      Notes
-    </div>
+const mockNotes = [
+  { id: "1", title: "Dance with dog", date: "12.01.2020" },
+  { id: "2", title: "Sleep with dog", date: "13.01.2020" },
+  { id: "3", title: "Eat with dog", date: "15.01.2020" },
+]
+
+export const Notes = () => {
+  return (
+    <>
+      {mockNotes.map((note, index) => (
+        <NotePreview key={`${note.id}-${index}`} id={note.id} title={note.title} date={note.date} />
+      ))}
+    </>
   )
 }
